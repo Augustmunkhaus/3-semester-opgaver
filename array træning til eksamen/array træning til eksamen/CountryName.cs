151953e7cc44529547a35fd7c2c8b98a23198e33
August <Munkhausaugust@gmail.com>
2024-01-15 21:16:18 +0100

    array træning

---
+++ array træning til eksamen/array træning til eksamen/CountryName.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_træning_til_eksamen
+{
+    public class CountryName
+
+    {
+        
+
+        public string[] countryArray = new string[] { "Norway", "Italy", "Belgium", "Germany", null, null, null, null, null, null };
+        public CountryName() { } 
+        
+
+        public bool ContainsCountry(string country)
+        {
+            return countryArray.Contains(country);
+            
+
+
+        }
+        public int Count()
+        {
+            
+                return countryArray.Count(x=> x != null);
+            
+        }
+              
+        public string RemoveCountry(string country)
+        {
+             for (int i = 0; i < countryArray.Length; i++)
+            {
+                if (countryArray[i] == country)
+            {
+                    countryArray[i] = null;
+            }
+                
+            
+            }
+            return $"landet: {country} er slettet";
+            
+        }
+
+        public string Add(int index, string country)
+        {
+            
+            for (int i = 0; i < countryArray.Length; i++)
+            {
+                if (countryArray[i] == null)
+                {
+                    index++;
+                }
+
+                else if (countryArray[i] == countryArray.Length)
+                {
+                    countryArray[i] = countryArray[0];
+                }
+                countryArray[i] = country;
+
+                
+
+
+            }
+        }
+    }
+} //$"landet:{country} er blevet slettet";
---
+++ array træning til eksamen/array træning til eksamen/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace array_træning_til_eksamen
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+            {
+            CountryName countries = new CountryName();
+
+            Console.WriteLine(countries.ContainsCountry("Norway"));
+            Console.WriteLine(countries.Count());
+            Console.WriteLine(countries.RemoveCountry("Belgium"));
+            Console.WriteLine(string.Join(", ", countries.countryArray));
+            Console.WriteLine(countries.Add(5,"Grønland"));
+
+        }
+            
+                /*
+                            public void Add(int index, string country)
+                                {
+                                int Index = 0;
+                                foreach(var i in country)
+                                {
+                                    if (Name != null)
+                                    {
+                                        Index++;
+                                    }
+                                    else if (Name == null)
+
+                                    {
+                                        Name.Replace(null, Name);
+                                    }
+                                    else Console.WriteLine("fejl");
+                                }
+                */
+                /*
+                public bool ContainsCountry(string country)
+                {
+                    int index = 0;
+                    while (index != null)
+                    {
+                        if (country.Name.Equals("Norway"))
+                        {
+                            return true;
+                        }
+
+                        index++;
+                    }
+
+                    return false;
+                }
+                */
+              
+        }
+
+            }
+
+        
+        
+    
+
+
+/*
+                public void RemoveCountry(string country)
+                {
+                    foreach (var i in countries)
+                    {
+                        if(countries.Equals(Name))
+                        {
+                            countries.Name.Replace(Name, null);
+                        }
+
+                    }
+                }
+
+                public int Count();
+                {
+                    int antal = 0;
+                       while(antal < countries.Length )
+                    {
+                        antal++;
+                    }
+                    return antal;
+                }
+        }
+        
+            
+                
+
+
+        static void Main(string[] args)
+        {
+                Console.WriteLine(Add(index,"USA"));
+
+            }
+
+        }
+    }
+
+
+
+
+    
+
+}*/
